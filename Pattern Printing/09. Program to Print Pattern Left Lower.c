32a12a5159972b12575345bf7ca665110a59e324
Shubham Sadashiv Patil <99086254+PatilShubham22@users.noreply.github.com>
2022-04-21 21:29:08 -0700

    Add files via upload

---
+++ Pattern Printing/09. Program to Print Pattern Left Lower.c
@@ -0,0 +1,28 @@
+/// Program to Print Pattern Left Lower
+
+#include <stdio.h>
+
+int main()
+{
+	int i = 0, j = 0, X = 0;
+
+	printf("\n Enter Row & Column Number = ");
+	scanf("%d", &X);
+
+	for (i = 1; i <= X; i++)
+	{
+		for (j = 1; j <= X; j++)
+		{
+			if (i >= j)
+			{
+				printf(" * ");
+			}
+			else
+			{
+				printf("   ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}

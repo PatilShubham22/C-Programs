f6683d50e5fda9919c042cb0b4086ee9d98b09ca
PatilShubham22 <shubham22398patil@gmail.com>
2022-09-02 17:12:31 +0530

    Did Minor Changes

--- Array With Function/12. Find Count of Zero Elements in That Array Using Micro - Using Function.c
+++ Array With Function/12. Find Count of Zero Elements in That Array Using Micro - Using Function.c
@@ -5,34 +5,34 @@
 #define Size 5
 
 void Accept_Array(int Num[ ]);
-int Zero_Count(int Nim1[ ]);
+int Zero_Count(int Num1[ ]);
 
 int main()
-{	
+{
 	int Bill[Size] = {0};
-	
+
 	Accept_Array(Bill);
-	
+
 	printf("\n Zero in Bill Amounts Are = %d",Zero_Count(Bill));
-	
+
 	return 0;
 }
 
 void Accept_Array(int Num[ ])
 {
 	int i = 0;
-			
+
 	for(i = 0; i < Size; i++)
 	{
 		printf("\n Enter Bill Amount %d = ", i + 1 );
 		scanf("%d",&Num[i]);
 	}
-}		
+}
 
 int Zero_Count(int Num1[ ])
 {
 		int i = 0, Zcnt = 0;
-		
+
 		for(i = 0; i < Size; i++)
 		{
 			if(Num1[i] == 0)
@@ -41,5 +41,4 @@ int Zero_Count(int Num1[ ])
 		    }
         }
       return Zcnt;
-}	     
-	
+}

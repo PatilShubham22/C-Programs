9f87607ccc023d20ab27983422aad107e74ccbc4
PatilShubham22 <shubham22398patil@gmail.com>
2022-09-02 17:22:16 +0530

    Did minor changes

--- Print Table/03. Program To Priint Table In Reverse Range - FIXED.c
+++ Print Table/03. Program To Priint Table In Reverse Range - FIXED.c
@@ -1,14 +1,13 @@
-/// Program To Priint Table In Reverse Range - FIXED
+/// Program To Print Table In Reverse Range - FIXED
 #include<stdio.h>
 #include<conio.h>
 
 int main()
 {
-		
 	int R = 0, C = 0, SNum = 20, ENum = 15;
-	
+
 	printf("\n\nTable As =>\n\n");
-	
+
 	for(R = 1; R <= 10; R++)
 	{
 		for(C = SNum; C >= ENum; C--)
@@ -16,11 +15,10 @@ int main()
 			printf(" %3d", (R * C));
 		}
 		printf("\n");
-	}									
-	
+	}
+
 return 0;
 }
-    
-        
-    
-    
+
+
+

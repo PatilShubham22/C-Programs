3180d4bbc6f118bda2f76a1c216be57d94e575e2
PatilShubham22 <shubham22398patil@gmail.com>
2022-09-06 09:18:59 +0530

    Added New File

---
+++ Pattern A-Z/G.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0, j = 0, x = 0;
+
+    printf("\n Enter Row & Column Number = ");
+    scanf("%d",&x);
+
+    for(i = 1; i <= x; i++)
+    {
+        for(j = 1; j <= x; j++)
+        {
+            if(j == 1 || i == 1 || i == x || i >= (x / 2 + 2) && j == x || j >= (x / 2 + 2) && i == (x / 2 + 2))
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+       printf("\n");
+    }
+
+  return 0;
+}

eeb5afab60a4e31dd769374d1660e2ed8e844166
PatilShubham22 <shubham22398patil@gmail.com>
2022-09-26 08:34:14 +0530

    Minor Changes

--- Square, Cube Programs/01. Program To Find Out Num1 Raise Num2 .c
+++ Square, Cube Programs/01. Program To Find Out Num1 Raise Num2 .c
@@ -1,31 +1,31 @@
-///Program To Find Out Num1 Raise Num2 
+///Program To Find Out Num1 Raise Num2
 #include<stdio.h>
 #include<conio.h>
 
 int main()
 {
   int Base = 0, Exp = 0, Pow = 0, Temp = 0;
-  
+
   printf("\n Enter Base Amount = ");
   scanf("%d",&Base);
-  
+
   printf("\n Enter Exponent Amount = ");
   scanf("%d",&Exp);
-  
+
   if(Base == 0 || Base == 1)
   {
   	Pow * Base;
   }
   else
-  {	
-     for(Pow = 1, Temp = Exp; Temp > 0; Temp--) 
-           {
-  	        Pow = Pow * Base;
-           }
-   }        
-  
+  {
+     for(Pow = 1, Temp = Exp; Temp > 0; Temp--)
+     {
+         Pow = Pow * Base;
+     }
+   }
+
   printf("\n Power (%d) ^ (%d) = %d", Base, Exp, Pow);
-  
-getch();    
+
+getch();
 return 0;
 }

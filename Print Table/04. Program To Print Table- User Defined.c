a1a64788b6f378b53130c925db6dc011925e92a2
PatilShubham22 <shubham22398patil@gmail.com>
2022-12-13 10:26:31 +0530

    Did Minor Changes

--- Print Table/04. Program To Print Table- User Defined.c
+++ Print Table/04. Program To Print Table- User Defined.c
@@ -5,21 +5,20 @@
 int main()
 {
 	int num = 0, cnt = 1;
-		
+
 	printf("\n\n Enter Any Integer Number To Get Table = ");
 	scanf("%d", &num);
-	
+
 	printf("\n Table For %d is =", num);
-	
+
 	while(cnt<=10)
 	{
 		printf("\n %d", (num * cnt));
 		cnt++;
-	}					
-	
+	}
+
 return 0;
 }
-    
-        
-    
-    
+
+
+
